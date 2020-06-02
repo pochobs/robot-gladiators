c6c031b3007b719443f47fbc9ef95326d0127a3c
Pochobs <pochobs@hotmail.com>
2020-06-02 12:07:23 -0500

    added assets and modified index.html

---
+++ assets/js/game.js
@@ -0,0 +1,9 @@
+// this creates a function named "fight"
+window.prompt("What is your robot's name");
+
+function fight() {
+    var playerName = window.prompt("What is your robot's name?");
+    // note the lack of quotation marks around playerName
+    window.alert(playerName);
+  }
+  fight();
