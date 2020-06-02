64c493901463d203446ce39c215997399b78e678
Pochobs <pochobs@hotmail.com>
2020-06-02 18:28:29 -0500

    add var, console log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,13 @@
-// this creates a function named "fight"
-// window.prompt("What is your robot's name");
-
 function fight() {
-    var playerName = window.prompt("What is your robot's name?");
-    // note the lack of quotation marks around playerName
-    window.alert(playerName);
-  }
-  
-  fight();
+  var playerName = window.prompt("What is your robot's name?");
+  // What is this?
+  console.log(playerName);
+  console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+var name = "your name";
+console.log(name);
+}
+fight();
