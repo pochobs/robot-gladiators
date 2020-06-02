86098bde75ffd1a50fa84ee1634d52a7c66f819a
Pochobs <pochobs@hotmail.com>
2020-06-02 12:34:25 -0500

    fixed bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,10 @@
 // this creates a function named "fight"
-window.prompt("What is your robot's name");
+// window.prompt("What is your robot's name");
 
 function fight() {
     var playerName = window.prompt("What is your robot's name?");
     // note the lack of quotation marks around playerName
     window.alert(playerName);
   }
+  
   fight();
