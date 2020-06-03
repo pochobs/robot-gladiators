a1dcec02b8242dde475f590bf522b03c69f8c97c
Pochobs <pochobs@hotmail.com>
2020-06-03 05:09:31 -0500

    add game2 and work 3.1 lesson

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,40 @@
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName
+window.alert(playerName);
 function fight() {
-  var playerName = window.prompt("What is your robot's name?");
-  // What is this?
-  console.log(playerName);
-  console.log("This logs a string, good for leaving yourself a message");
-// this will do math and log 20
-console.log(10 + 10);
-// what is this?
-console.log("Our robot's name is " + playerName);
-var name = "your name";
-console.log(name);
+  window.alert("The fight has begun!");
 }
+
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+  
+  // Alert users that they are starting the round
+  window.alert{"Welcome to Robot Gladiators!");
+
+//Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+enemyHealth = enemyHealth - playerAttack;
+
+// Log a resulting message to the console so we know that it worked.
+console.log(
+  playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+);
+
+// check enemy's health
+if (enemyHealth <= 0) {
+  window.alert(enemyName + " has died!");
+} 
+else {
+  window.alert(enemyName + " still has " + enemyHealth + " health left.");
+}
+  }
 fight();
